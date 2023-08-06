2ff6171a51c9a0373262fb84ce464c55b643de7e
sonalimalviya08 <75033150+sonalimalviya08@users.noreply.github.com>
2023-08-06 13:06:24 +0530

    Add files via upload

---
+++ predict_response.py
@@ -0,0 +1,37 @@
+#Text Data Preprocessing Lib
+import nltk
+
+import json
+import pickle
+import numpy as np
+import random
+
+ignore_words = ['?', '!',',','.', "'s", "'m"]
+
+# Model Load Lib
+import tensorflow
+from data_preprocessing import get_stem_words
+
+model = tensorflow.keras.models.load_model('./chatbot_model.h5')
+
+# Load data files
+intents = json.loads(open('./intents.json').read())
+words = pickle.load(open('./words.pkl','rb'))
+classes = pickle.load(open('./classes.pkl','rb'))
+
+
+def preprocess_user_input(user_input):
+
+    
+   
+    
+
+def bot_class_prediction(user_input):
+
+    
+
+
+def bot_response(user_input):
+
+   
+
